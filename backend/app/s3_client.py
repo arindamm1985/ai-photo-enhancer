f14926c87ee6bd4c0c7dace99a45c7ac4c42c135
arindamm1985 <41725902+arindamm1985@users.noreply.github.com>
2025-04-21 14:16:48 +0530

    direct buxketname coding

--- backend/app/s3_client.py
+++ backend/app/s3_client.py
@@ -7,7 +7,7 @@ s3 = boto3.client(
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
 )
-BUCKET = os.getenv("S3_BUCKET")
+BUCKET = "ai-photo-enhancer-bucket"
 
 def upload_file(file_path, s3_key):
     s3.upload_file(file_path, BUCKET, s3_key)
